5821bdffdb6c6c5829399f4a460957c4b6a782f3
Alexandre-27 <alexandre24102000@gmail.com>
2024-08-11 19:16:36 +0200

    Removing the form to replace it with a phrase and removing the handleSubmitNewArt function

--- frontend/src/components/MapForm.jsx
+++ frontend/src/components/MapForm.jsx
@@ -46,134 +46,14 @@ function MapForm() {
     getInfos();
   }, []);
 
-  const handleSubmitNewArt = async () => {
-    try {
-      const backendUrl =
-        import.meta.env.VITE_BACKEND_URL || "http://localhost:3310";
-
-      const locationData = {
-        city: "Paris",
-        country: "France",
-        post_code: 75013,
-        street: "Rue Gérard",
-        street_number: 62,
-        latitude: 48.8293,
-        longitude: 2.352,
-      };
-
-      const locationResponse = await axios.post(
-        `${backendUrl}/api/locations`,
-        locationData
-      );
-
-      if (!locationResponse.data) {
-        console.error(
-          "Erreur lors de la création de l'emplacement :",
-          locationResponse.data
-        );
-      }
-
-      const artworkData = {
-        title: "Vivre c'est de la bombe",
-        picture: "https://shorturl.at/civKS",
-        description:
-          "Une nouvelle collaboration entre Jace et Miss.Tic sur un mur de la Butte aux Cailles",
-        artist_id: 42,
-        user_id: 52,
-        general_gallery: false,
-        reported: false,
-        location_id: 159,
-      };
-
-      const artworkResponse = await axios.post(
-        `${backendUrl}/api/artworks`,
-        artworkData
-      );
-
-      if (!artworkResponse.data) {
-        throw new Error("Erreur lors de la création de l'œuvre d'art");
-      }
-    } catch (error) {
-      console.error("Erreur lors de la soumission des données.", error.message);
-    }
-  };
-
-  // const handleSubmitCapture = async () => {
-  //   try {
-  //     if (selectedArtworkId !== null && artistSelect !== null) {
-  //       const captureData = {
-  //         user_id: user.id,
-  //         artwork_id: selectedArtworkId,
-  //       };
-
-  //       await axios.post(
-  //         `${import.meta.env.VITE_BACKEND_URL}/api/captures`,
-  //         captureData
-  //       );
-  //     }
-  //   } catch (error) {
-  //     console.error("Erreur lors de la soumission des données.", error.message);
-  //   }
-  // };
-
   const handleReturn = () => {
     switch (user.is_administrator) {
       case 1:
         return (
           <>
-            <h2 className="admin-mode">Ajouter une œuvre</h2>
+            <h2 className="player-mode">Ajouter une capture</h2>
 
-            <form className="content">
-              <div className="parent-div">
-                <div className="left-container">
-                  <div className="align-div">
-                    <p>
-                      Nom de l'artiste <span className="red-span">*</span>
-                    </p>
-                    <input name="artistName" />
-                  </div>
-                  <div className="align-div">
-                    <p>Nom de l'œuvre</p>
-                    <input name="title" />
-                  </div>
-                  <div className="align-div">
-                    <p>
-                      Adresse <span className="red-span">*</span>
-                    </p>
-                    <input name="street" />
-                  </div>
-                  <div className="align-div">
-                    <p>
-                      Ville <span className="red-span">*</span>
-                    </p>
-                    <input name="city" />
-                  </div>
-                </div>
-                <div className="right-container">
-                  <div className="align-div">
-                    <p>
-                      Description de l'œuvre <span className="red-span">*</span>
-                    </p>
-                    <input name="description" className="desc-inp" />
-                  </div>
-                  <div className="align-div">
-                    <p>
-                      À propos de l'artiste
-                      <span className="red-span"> (bonus)</span>
-                    </p>
-                    <input name="artistBio" className="about-inp" />
-                  </div>
-                </div>
-              </div>
-            </form>
-            <Dropzone selectedArtworkId={selectedArtworkId} />
-            <button
-              type="button"
-              className="admin-mode"
-              onClick={handleSubmitNewArt}
-            >
-              Soumettre
-            </button>
+            <p>Se connecter en tant qu'utilisateur pour jouer</p>
           </>
         );
       case 0:
